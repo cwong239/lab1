d820a1a478a29c9e95cf19970e1ab356ef0369d6
Christopher Wong <cwong239@calpoly.edu>
2023-10-02 10:40:01 -0700

    finished 1-3

--- section4.py
+++ section4.py
@@ -1,3 +1,24 @@
-class TestCase:
+class Feet2Meters:
     def __init__(self):
         pass
+    def f2m(self,feet):
+        try:
+            return feet * 0.3048
+        except:
+            return "Enter a number please"
+
+
+
+class Note:
+    def __init__(self, p, d):
+        self.pitch = p
+        self.duration = d
+
+    def getPitch(self):
+        return self.pitch
+    def getDuration(self):
+        return self.duration
+
+def up_one_octave(self, note):
+    return note.getPitch()*2
+
